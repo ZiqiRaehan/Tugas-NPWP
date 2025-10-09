2a58c3b586727c51f8e914dee28e4d4ff33b0a3b
Ziqi Raehan <buatnyimpendata123@example.com>
2025-10-10 00:32:10 +0700

    confog production

--- next.config.ts
+++ next.config.ts
@@ -6,8 +6,7 @@ const nextConfig: NextConfig = {
     unoptimized: true, // ✅ biar next/image tetap jalan di hosting biasa (tanpa server)
   },
   
-  basePath: "/prod",
-  assetPrefix: "/prod/",
+ 
 };
 
 export default nextConfig;
